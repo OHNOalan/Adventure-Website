716c49bd913b82a826e7e4636a419ba4aba44f14
ALANLEE <alanlee@ALANLEEdeMacBook-Pro.local>
2022-05-18 00:33:09 -0400

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import Home from '/Users/alanlee/Desktop/CS program/Web learning/React website/react-website-yt-1/src/components/pages/Home.js';
+import Home from './components/pages/Home';
 // import { BrowserRouter as Router} from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
